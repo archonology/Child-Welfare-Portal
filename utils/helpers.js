1795c02e262ba25177c71a3dcd059d282d8c765f
Reed Meher <reed.meher@gmail.com>
2022-11-10 09:49:35 -0600

    added helper files for date and email generating

---
+++ utils/helpers.js
@@ -0,0 +1,11 @@
+module.exports = {
+    // to use for formatting date/time as needed
+    format_time: (date) => {
+      return date.toLocaleTimeString();
+    },
+    format_date: (date) => {
+      return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
+        new Date(date).getFullYear()
+      }`;
+    },
+  };
