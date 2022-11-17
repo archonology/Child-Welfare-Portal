53a1b16e71d3543330b869262d8a8a45e2f24f47
Reed Meher <reed.meher@gmail.com>
2022-11-17 11:03:43 -0600

    adding models and index exports

--- models/index.js
+++ models/index.js
@@ -1,7 +1,46 @@
+//user and subscriber tables
 const User = require('./User');
 const Subscriber = require('./Subscriber');
+//resource tables
+const Abuse = require('./Abuse');
+const Advocacy = require('./Advocacy');
+const CasePlanning = require('./CasePlanning');
+const Court = require('./Court');
+const CpLaw = require('./CpLaw');
+const DataPrivacy = require('./DataPrivacy');
 const Diversity = require('./Diversity');
+const EdNeglect = require('./EdNeglect');
+const Ethics = require('./Ethics');
+const FosterCare = require('./FosterCare');
+const History = require('./History');
+const Incarceration = require('./Incarceration');
+const Interviewing = require('./Interviewing');
+const Ive = require('./Iv-e');
+const MentalHealth = require('./MentalHealth');
+const Parenting = require('./Parenting');
+const Ssis = require('./SSIS');
+const SubstanceAbuse = require('./SubstanceAbuse');
 
+//user and subscriber exports
 module.exports = User;
 module.exports = Subscriber;
+
+//resource table exports
+module.exports = Abuse;
+module.exports = Advocacy;
+module.exports = CasePlanning;
+module.exports = Court;
+module.exports = CpLaw;
+module.exports = DataPrivacy;
 module.exports = Diversity;
+module.exports = EdNeglect;
+module.exports = Ethics;
+module.exports = FosterCare;
+module.exports = History;
+module.exports = Incarceration;
+module.exports = Interviewing;
+module.exports = Ive;
+module.exports = MentalHealth;
+module.exports = Parenting;
+module.exports = Ssis;
+module.exports = SubstanceAbuse;
